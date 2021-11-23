edbe7a844416c3d4f1c6246906815fb88410268c
UserXM230121 <UserXM230121@DESKTOP-591DAC7>
2021-11-23 18:55:23 +0600

    Добавьте файлы проекта.

---
+++ lab 3.3 15v/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace lab_3._3_15v
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите n:");
+                int n = int.Parse(Console.ReadLine());
+                Console.WriteLine("Введите x:");
+                double x = double.Parse(Console.ReadLine());
+                double S = 0;
+                for (int i = 1; i <= n; i++)
+                {
+                    long F = 1;
+                    for (int j = 3; j < 2 * i - 1; j++) F *= j;
+                    S += Math.Pow(x, F) / F;
+                }
+                Console.WriteLine("S={0:F2}", S);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
